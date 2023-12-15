282b4af74e2f1a7f8a62ec843c470ebdfb3632e4
kodai <kodai@MSI>
2023-12-15 14:31:51 +0900

    中断

--- main.cpp
+++ main.cpp
@@ -22,17 +22,29 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	Object player =
 	{
-		{640,360},
+		{640,660},
 		{5,5}
 	};
+	int playerradius = 40;
 
 	Object enemy =
 	{
 		{100,100},
 		{6,6}
 	};
-
 	int radius = 40;
+
+	//弾
+	Object bullet =
+	{
+		{0,0},
+		{5,5}
+	};
+	int Bulletraedius = 20;
+
+	bool bulletflag = false;
+
+
 
 
 
@@ -53,6 +65,67 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+		if (keys[DIK_W])
+		{
+			player.position.y -= player.speed.y;
+		}
+		if (keys[DIK_A])
+		{
+			player.position.x -= player.speed.x;
+		}
+		if (keys[DIK_S])
+		{
+			player.position.y += player.speed.y;
+		}
+		if (keys[DIK_D])
+		{
+			player.position.x += player.speed.x;
+		}
+
+		//敵移動
+		enemy.position.x += enemy.speed.x;
+
+		if (enemy.position.x + 40 >= 1280)
+		{
+			enemy.speed.x = enemy.speed.x * -1;
+		}
+
+		if (enemy.position.x - 40 <= 0)
+		{
+			enemy.speed.x = enemy.speed.x * -1;
+		}
+
+		//弾発射
+		if (bulletflag == false)
+		{
+			if (keys[DIK_SPACE] && preKeys[DIK_SPACE] == 0)
+			{
+				bulletflag = true;
+
+				bullet.position.x = player.position.x;
+				bullet.position.y = player.position.y;
+			}
+		}
+
+		if (bulletflag == true)
+		{
+			bullet.position.y -= bullet.speed.y;
+
+			if (bullet.position.x <= enemy.position.x + radius &&
+				enemy.position.x <= bullet.position.x + Bulletraedius &&
+				bullet.position.y <= enemy.position.y + radius &&
+				enemy.position.y <= bullet.position.y + Bulletraedius)
+			{
+				bulletflag = false;
+			}
+		}
+
+		if (bullet.position.y <= 0)
+		{
+			bulletflag = false;
+		}
+
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -60,6 +133,15 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		if (bulletflag == true)
+		{
+			Novice::DrawBox(bullet.position.x + 5, bullet.position.y, Bulletraedius, Bulletraedius, 0.0f, BLACK, kFillModeSolid);
+		}
+
+		Novice::DrawBox(player.position.x, player.position.y, playerradius, playerradius, 0.0f, WHITE, kFillModeSolid);
+
+		Novice::DrawBox(enemy.position.x, enemy.position.y, radius, radius, 0.0f, RED, kFillModeSolid);
 
 		///
 		/// ↑描画処理ここまで
